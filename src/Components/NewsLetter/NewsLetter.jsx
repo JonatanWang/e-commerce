b16b609907a1931a1b7947f9187af7df29d0f36b
JonatanWang <jonatan.wang@gmail.com>
2024-06-10 21:16:10 +0200

    Add Items, Offers, NewCollections, Popular & NewsLetter

---
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,17 @@
+import "./NewsLetter.css";
+import React from "react";
+
+export const NewsLetter = () => {
+  return (
+    <div className="newsletter">
+      <h1>GET EXCLUSIVE OFFERS ON YOUR EMAIL</h1>
+      <p>Subscribe to our newsletter and stay updated</p>
+      <div>
+        <input type="email" placeholder="Your Email id" />
+        <button>Subscribe</button>
+      </div>
+    </div>
+  );
+};
+
+export default NewsLetter;
